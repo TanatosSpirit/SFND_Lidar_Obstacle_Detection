0a7612f456fbebe5adde7a89ad91e4f102f64ea7
Andrei Liutov <lytovandrei@gmail.com>
2020-10-24 01:18:30 +0300

    Add point cloud

--- src/environment.cpp
+++ src/environment.cpp
@@ -45,10 +45,11 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     bool renderScene = true;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
-    // TODO:: Create lidar sensor 
+    // TODO:: Create lidar sensor
     auto* lidar = new Lidar(cars, 0);
     pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
-    renderRays(viewer, lidar->position, inputCloud);
+//    renderRays(viewer, lidar->position, inputCloud);
+    renderPointCloud(viewer, inputCloud, "Cloud");
 
     // TODO:: Create point processor
   
