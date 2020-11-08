13aff7674b5ad309469d463bcc8dfec98d240a41
Andrei Liutov <lytovandrei@gmail.com>
2020-11-08 15:50:04 +0300

    Add ransac

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,7 +42,7 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor
@@ -56,7 +56,7 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
 
     std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr,
               pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor->SegmentPlane(inputCloud,
-                                                                                               100,
+                                                                                               2,
                                                                                                0.2);
     renderPointCloud(viewer,segmentCloud.first,"obstCloud",Color(1,0,0));
     renderPointCloud(viewer,segmentCloud.second,"planeCloud",Color(0,1,0));
